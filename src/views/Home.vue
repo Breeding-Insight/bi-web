f5d896476d93adcfa053dc38d4c163d69e37fc15
Nick Palladino <np398@cornell.edu>
2019-12-23 13:58:10 -0500

    Rudimentary home page layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="columns is-mobile">
+      <img class="column is-2 is-offset-5" alt="Breeding Insight Logo" src="../assets/img/bi-logo.svg">
+    </div>
+    <nav class="level">
+      <p class="level-item has-text-centered">Combining genomics and informatics to accelerate genetic gains</p>
+    </nav>
+    <div class="columns">
+      <div class="column">
+        <p>Part of a Breeding Insight program?</p>
+        <button class="button is-primary">LOG IN</button>
+      </div>
+      <div class="column">
+        <h5 class="title is-5">Our Mission</h5>
+        <p>By combining genomics with the powerful tools of informatics, breeding for genetic gain has undergone a revolution. But due to high-costs, some smaller breeding programs have been left behind. The mission of the Breeding Insight team is to make high-tech breeding a reality for smaller programs.</p>
+        <p>Breeding Insight is funded by the U.S. Department of Agriculture (USDA) Agricultural Research Service (ARS) through Cornell University. The USDA ARS delivers scientific solutions to national and global agricultural challenges. As a global leader in agricultural discovery through scientific excellence, ARS is committed to delivering cutting-edge, scientific tools and innovative solutions for American farmers, producers, industry, and communities to support the nourishment and well-being of all people; sustaining our nation’s agroecosystems and natural resources; and ensuring the economic competitiveness and excellence of our agriculture.</p>
+      </div>
+    </div>
+    <div class="modal" id="loginModal">
+    </div>
   </div>
+
+  <!-- <div class="home">
+    <img alt="Breeding Insight Logo" src="../assets/img/bi-logo.svg">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
 </template>
 
-<script>
+
+<!-- <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -15,4 +38,4 @@ export default {
     HelloWorld
   }
 }
-</script>
+</script> -->
