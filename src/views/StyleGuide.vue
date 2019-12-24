1d297f444f0c4855401b1fd40abbda573ff59bd5
Liz Woods <eaw82@cornell.edu>
2019-12-24 10:01:44 -0500

    add typography examples

--- src/views/StyleGuide.vue
+++ src/views/StyleGuide.vue
@@ -2,5 +2,14 @@
   <section class="style-guide section">
     <h1 class="title">Breeding Insight Style Guide</h1>
     <h2 class="subtitle">Living documentation of best practices and style examples</h2>
+    <p>If the next element after a subtitle is a paragraph, proper spacing will occur.</p>
+    <h2 class="title is-4">Typestyles</h2>
+    <p>Note that using an element with <code>class="title"</code> and that same element with <code>class="subtitle"</code> produces different results.
+    <p>Example:</p>
+    <h3 class="title">This is an h3 element with a class=title</h3>
+    <h3 class="subtitle">This is an h3 element with a class=subtitle</h3>
+    <p>Also note that the sizing of headers does not depend on the heading level of the element. Size is controlled by the <code>is-1</code>, <code>is-2</code>, <code>is-3</code> classes and so on.</p>
+    <p>I am not sure why there is not a bottom margin on a paragraph before a header. Will check on that.</p>
+    <p>More information on typestyles to come.</p>
   </section>
 </template>
