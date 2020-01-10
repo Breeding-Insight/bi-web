f166e60046f7aaf859a862508b455832c9a9d602
Chris T <ct447@cornell.edu>
2020-01-10 11:45:02 -0500

    add table rows

--- src/views/UserManagement.vue
+++ src/views/UserManagement.vue
@@ -2,20 +2,27 @@
     <div class="usermanagement">
         <div class="container is-fluid">
             <section class="section">
-                <div class="level is-large content">
-                    <h1 class="content is-large">User Management</h1>
-                </div>
+                <h1 class="title">User Management</h1>
+
                 <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
                     <thead>
                         <tr>
                             <th>User Name</th>
                             <th>Email</th>
                             <th>Roles</th>
+                            <th>Manage User</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        
-
+                    <tbody v-bind:key="user.email" v-for="user of users">
+                        <tr class="is-vcentered">
+                            <td>{{ user.name }}</td>
+                            <td>{{ user.email }}</td>
+                            <td>{{ user.roles }}</td>
+                            <td>
+                                <b-button class="is-primary">Edit</b-button>
+                                <b-button class="is-danger">Delete</b-button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </section>
@@ -23,10 +30,14 @@
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
   @Component
   export default class UserManagement extends Vue {
+
+      public users: Array<Object> = [{'name': 'Chris', 'email': 'ct447@cornell.edu', roles: 'Admin'}];
+
+
   }
 </script>
