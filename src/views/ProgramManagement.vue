0f2c074fcc2a627194ea0c2dcc0356c2abe64ad2
Chris T <ct447@cornell.edu>
2020-02-17 10:34:59 -0500

    [PRO-43] Separated menus into components

--- src/views/ProgramManagement.vue
+++ src/views/ProgramManagement.vue
@@ -1,47 +1,15 @@
+import {EditIcon} from "vue-feather-icons";import {DeleteIcon} from "vue-feather-icons";import {CheckSquareIcon} from "vue-feather-icons";import {XIcon} from "vue-feather-icons";
 <template>
-  <div class="program-management">
-    <div class="container is-fluid">
-      <div class="level header-title">
-        <div class="level-left">
-          <div class="level-item">
-            <a href="/">
-              <img 
-              src="../assets/img/bi-logo.svg" 
-              alt="Breeding Insight home" 
-              width="232" 
-            >
-            </a>
-          </div>
-        </div>
-        <div class="level-right">
-          <h1 class="title has-text-primary">Program Name Here</h1>
-        </div>
-      </div>
-      <section class="section no-padding">
-        <div class="columns">
-          <div class="column is-one-quarter side-menu">
-            <b-menu>
-              <b-menu-item label="Trials and Experiments"></b-menu-item>
-              <b-menu-item label="Germplasm Inventory"></b-menu-item>
-              <b-menu-item label="Ontology Management"></b-menu-item>
-              <b-menu-item label="Labels"></b-menu-item>
-              <b-menu-item label="Reports"></b-menu-item>
-              <b-menu-item label="Program Management"></b-menu-item>
-            </b-menu>
-          </div>
-          <div class="column">
-            <h1 class="title">Program Management</h1>
-          </div>
-        </div>
-      </section>
-    </div>
-  </div>
+    <h1 class="title">Program Management</h1>
 </template>
 
 <script lang="ts">
   import { Component, Prop, Vue } from 'vue-property-decorator'
+  import SideBarMaster from '@/components/layouts/SideBarLayout.vue'
 
-  @Component
+  @Component({
+    components: {SideBarMaster}
+  })
   export default class ProgramManagement extends Vue {
   }
 </script>
