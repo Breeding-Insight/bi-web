47349c09a7399fca251a1890fc7879fb0e634184
Chris T <ct447@cornell.edu>
2020-02-18 13:43:59 -0500

    [PRO-43] finished styling for navigation

--- src/components/layouts/SideBarLayout.vue
+++ src/components/layouts/SideBarLayout.vue
@@ -1,82 +1,111 @@
 <template>
-  <div>
-    <div class="level header-title is-marginless">
-      <div class="level-left">
-        <div class="level-item">
-          <a href="/">
-            <img
-                src="../../assets/img/bi-logo.svg"
-                alt="Breeding Insight home"
-                width="232"
-            >
-          </a>
-        </div>
-      </div>
-      <div class="level-right">
-        <h1 class="title has-text-primary">Program Name Here</h1>
-      </div>
-    </div>
-
-    <div class="columns is-marginless">
-      <aside class="menu side-menu column is-one-fifth">
-        <ul class="menu-list">
-          <li><router-link to="/userhome">Home</router-link></li>
-          <li><router-link to="/usermanagement">Manage Users</router-link></li>
-          <li><a>Trials and Experiments</a></li>
-          <li><a>Germplasm Inventory</a></li>
-          <li><a>Ontology Management</a></li>
-          <li><a>Labels</a></li>
-          <li><a>Reports</a></li>
-          <li>
-            <a
-              v-bind:class="{ 'is-active': programManagementActive }"
-              @click="programManagementActive = !programManagementActive"
-            >
-              Program Management
-              <MoreVerticalIcon v-if="!programManagementActive" class="is-pulled-right"></MoreVerticalIcon>
-              <MoreHorizontalIcon v-if="programManagementActive" class="is-pulled-right"></MoreHorizontalIcon>
+  <div class="sidebarlayout">
+    <header>
+      <div class="level header-title is-marginless is-mobile">
+        <div class="level-left">
+          <div class="level-item is-hidden-touch">
+            <a href="/">
+              <img
+                  src="../../assets/img/bi-logo.svg"
+                  alt="Breeding Insight home"
+                  width="232"
+              >
             </a>
-            <ul v-show="programManagementActive">
-              <li><a>Locations</a></li>
-              <li><a>Users</a></li>
-              <li><a>Roles</a></li>
-              <li><a>Permissions</a></li>
-            </ul>
-          </li>
-        </ul>
-      </aside>
-      <div class="column">
-        <div class="level">
-          <div class="level-left"></div>
-          <div class="level-right">
-            <div class="level-item">
-              <p>Logged in as <b>{{username}}</b></p>
-            </div>
-            <div class="level-item">
-              <button class="button is-outlined is-primary" @click="$emit('logout')">Log out</button>
-            </div>
+          </div>
+          <div class="level-item is-hidden-desktop is-pulled-left">
+            <a role="button"
+               class="navbar-hamburger has-text-black"
+               aria-label="menu"
+               aria-expanded="false"
+               @click="sideMenuShownMobile = !sideMenuShownMobile"
+            >
+              <MenuIcon></MenuIcon>
+            </a>
           </div>
         </div>
-        <section class="section">
-          <slot></slot>
-        </section>
+        <div class="level-right">
+          <h1 class="title has-text-primary">Program Name</h1>
+        </div>
       </div>
+    </header>
+
+    <div class="columns is-marginless">
+      <div
+          class="column side-menu is-one-quarter"
+          v-bind:class="{ 'is-hidden-touch': !sideMenuShownMobile }"
+      >
+        <nav role="navigation" aria-label="main navigation">
+          <aside id="sideMenu" class="menu">
+            <ul class="menu-list">
+              <li><router-link to="/userhome">Home</router-link></li>
+              <li><router-link to="/usermanagement">Manage Users</router-link></li>
+              <li><a>Trials and Experiments</a></li>
+              <li><a>Germplasm Inventory</a></li>
+              <li><a>Ontology Management</a></li>
+              <li><a>Labels</a></li>
+              <li><a>Reports</a></li>
+              <li>
+                <a
+                    v-bind:class="{ 'is-active': programManagementActive }"
+                    @click="programManagementActive = !programManagementActive"
+                >
+                  Program Management
+                  <MoreVerticalIcon v-if="!programManagementActive" class="is-pulled-right"></MoreVerticalIcon>
+                  <MoreHorizontalIcon v-if="programManagementActive" class="is-pulled-right"></MoreHorizontalIcon>
+                </a>
+                <ul v-show="programManagementActive">
+                  <li><a>Locations</a></li>
+                  <li><a>Users</a></li>
+                  <li><a>Roles</a></li>
+                  <li><a>Permissions</a></li>
+                </ul>
+              </li>
+            </ul>
+          </aside>
+        </nav>
+      </div>
+
+      <main>
+        <div class="column">
+          <div class="level is-mobile">
+            <div class="level-left"></div>
+            <div class="level-right">
+              <div class="level-item">
+                <p>Logged in as <b>{{username}}</b></p>
+              </div>
+              <div class="level-item">
+                <button class="button is-outlined is-primary" @click="$emit('logout')">Log out</button>
+              </div>
+            </div>
+          </div>
+          <section class="section">
+            <slot></slot>
+          </section>
+        </div>
+      </main>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import {Component, Prop, Vue} from 'vue-property-decorator'
-  import { MoreVerticalIcon, MoreHorizontalIcon } from 'vue-feather-icons'
+  import {Component, Prop, Watch, Vue} from 'vue-property-decorator'
+  import { MoreVerticalIcon, MoreHorizontalIcon, MenuIcon } from 'vue-feather-icons'
 
 
   @Component( {
-    components: {MoreVerticalIcon, MoreHorizontalIcon}
+    components: {MoreVerticalIcon, MoreHorizontalIcon, MenuIcon}
   })
   export default class SideBarMaster extends Vue {
     programManagementActive: boolean =  true;
+    sideMenuShownMobile: boolean = false;
+
     @Prop()
     username!: string;
+
+    @Watch('$route')
+    onUrlChange(){
+      this.sideMenuShownMobile = false;
+    }
   }
 
 </script>
