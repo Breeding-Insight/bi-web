9fb73dd11b2cfc9d290ef5855ec4207eaa671573
Nick Palladino <np398@cornell.edu>
2020-02-20 10:00:31 -0500

    [PRO-43] Added margin, changed background color

--- src/components/modals/WarningModal.vue
+++ src/components/modals/WarningModal.vue
@@ -1,30 +1,31 @@
 <template>
   <b-modal v-bind:active.sync="active">
     <div class="modal-card" style="width: 400px">
-      <div class="message is-danger">
-        <div class="message-body">
-          <div class="columns">
-            <div class="column is-narrow">
-              <alert-triangle-icon size="1.5x"></alert-triangle-icon>
+      <section class="modal-card-body">
+        <div class="message is-danger">
+          <div class="message-body">
+            <div class="columns">
+              <div class="column is-narrow">
+                <alert-triangle-icon size="1.5x"></alert-triangle-icon>
+              </div>
+              <div class="column">
+                <h3 class="is-5 title has-text-danger">
+                  {{msg_title}}
+                </h3>
+              </div>
             </div>
-            <div class="column">
-              <h3 class="is-size-5 has-text-weight-bold">
-                {{msg_title}}
-              </h3>
-            </div>
-          </div>
-          <p>
-            {{msg_body}}
-          </p>
-          <div class="columns">
-            <div class="column is-whole has-text-centered buttons">
-              <button v-on:click="$emit('submit')" class="button is-danger"><strong>{{btn_submit_txt}}</strong></button>
-              <button v-on:click="$emit('cancel')" class="button">{{btn_cancel_txt}}</button>
+            <p>
+              {{msg_body}}
+            </p>
+            <div class="columns">
+              <div class="column is-whole has-text-centered buttons">
+                <button v-on:click="$emit('submit')" class="button is-danger"><strong>{{btn_submit_txt}}</strong></button>
+                <button v-on:click="$emit('cancel')" class="button">{{btn_cancel_txt}}</button>
+              </div>
             </div>
           </div>
         </div>
-        
-      </div>
+      </section>
     </div>
   </b-modal>
 </template>
