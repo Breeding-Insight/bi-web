5ed481e38aea2151c5f93be40e40656288cc05ff
Chris T <ct447@cornell.edu>
2020-02-20 10:14:46 -0500

    [PRO-43] fixed prop mutation issue with modals

---
+++ src/components/modals/InfoModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <BaseModal
+    v-bind:active="active"
+    v-on:close-modal="$emit('close-modal')"
+  >
+    <slot></slot>
+  </BaseModal>
+
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import BaseModal from '@/components/modals/BaseModal.vue';
+
+  @Component({
+    components: {BaseModal}
+  })
+  export default class InfoModal extends Vue {
+    @Prop()
+    active!: boolean;
+  }
+
+</script>
