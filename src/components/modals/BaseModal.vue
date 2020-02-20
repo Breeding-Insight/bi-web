99047332c542214f8e43ba74e5e35f03b69cb731
Chris T <ct447@cornell.edu>
2020-02-20 09:29:49 -0500

    [PRO-43] styled login modal

---
+++ src/components/modals/BaseModal.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div
+    class="modal"
+    v-bind:class="{ 'is-active': active }"
+>
+  <div class="modal-background" />
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <div class="modal-card-title" />
+      <button
+          class="delete"
+          aria-label="close"
+          @click="$emit('closeModal')"
+      />
+    </header>
+    <section class="modal-card-body has-text-centered">
+      <slot></slot>
+    </section>
+  </div>
+</div>
+
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class BaseModal extends Vue {
+    @Prop()
+    public active: boolean = false;
+  }
+
+</script>
