8878fa901091744a0b83699d3fe5a30910aa9742
Chris T <ct447@cornell.edu>
2020-03-25 13:15:49 -0400

    [INF-87] added scaffolded unit tests

---
+++ tests/unit/components/newDataForm.spec.ts
@@ -0,0 +1,8 @@
+describe('Input error is displayed appropriately when validation error occurs.');
+
+describe('Events emitted properly.', () => {
+
+  it('Emits submit event with edited object on editing save', () => {});
+
+})
+
---
+++ tests/unit/models/user.spec.ts
@@ -0,0 +1,38 @@
+describe('User service returns expected types.', () => {
+
+  it('Returns array of programs for getAll', () => {});
+
+  it('Returns empty response for delete', () => {});
+
+  it('Returns single updated program for update', () => {});
+
+  it('Returns single updated program for update', () => {});
+
+});
+
+describe('User service create handles exceptions appropriately.', () => {
+
+  it('Returns a rejected promise with status code for getAll', () => {});
+
+  it('Returns a rejected promise with empty response for getAll, unreachable server', () => {});
+
+  it('Returns a rejected promise with no return object for getALL, method error', () => {});
+
+  it('Returns a rejected promise with status code for update', () => {});
+
+  it('Returns a rejected promise with empty response for update, unreachable server', () => {});
+
+  it('Returns a rejected promise with no return object for update, method error', () => {});
+
+  it('Returns a rejected promise with status code for create', () => {});
+
+  it('Returns a rejected promise with empty response for create, unreachable server', () => {});
+
+  it('Returns a rejected promise with no return object for create, method error', () => {});
+
+  it('Returns a rejected promise with status code for delete', () => {});
+
+  it('Returns a rejected promise with empty response for delete, unreachable server', () => {});
+
+  it('Returns a rejected promise with no return object for delete, method error', () => {});
+});
