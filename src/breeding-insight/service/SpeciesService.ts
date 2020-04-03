d28ec901bb11ca0f7b09ef76be72b698dbb7bb6a
Nick Palladino <np398@cornell.edu>
2020-04-03 16:58:48 -0400

    [INF-100] Added species api calls

--- src/breeding-insight/service/SpeciesService.ts
+++ src/breeding-insight/service/SpeciesService.ts
@@ -4,7 +4,20 @@ import {Species} from "@/breeding-insight/model/Species";
 export class SpeciesService{
 
   static getAll(): Promise<Species[]> {
-    return SpeciesDAO.getAll();
+    return new Promise<Species[]>(((resolve, reject) => {
+
+      SpeciesDAO.getAll().then((biResponse) => {
+
+        // Parse our users into the vue users param
+        const species = biResponse.result.data.map((species: any) => {
+          return new Species(species.id, species.commonName);
+        });
+
+        resolve(species);
+
+      }).catch((error) => reject(error));
+
+    }));
   }
 
 }
