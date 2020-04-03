c1ed4774d6b436a4357905a0071a948b30cd30ef
Nick Palladino <np398@cornell.edu>
2020-04-03 17:02:09 -0400

    [INF-100] Added program to vuex state

--- src/store/types.ts
+++ src/store/types.ts
@@ -4,9 +4,15 @@ export interface User {
   roles: string[];
 }
 
+export interface Program {
+  id: string;
+  name: string;
+}
+
 export interface RootState {
   loggedIn: boolean;
   user?: User;
+  program?: Program;
   apiError: boolean;
   apiUnavailable: boolean;
   loginFailed: boolean;
