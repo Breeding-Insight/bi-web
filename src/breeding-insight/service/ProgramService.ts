c9e41590c40e26a61447d97046d1c34d1b09fa84
Nick Palladino <np398@cornell.edu>
2020-04-07 09:58:33 -0400

    [INF-100] Updated admin programs table

--- src/breeding-insight/service/ProgramService.ts
+++ src/breeding-insight/service/ProgramService.ts
@@ -5,27 +5,78 @@ export class ProgramService {
 
   static create(program: Program): Promise<Program> {
     //TODO: Check everything is good
-    return ProgramDAO.create(program);
+    return new Promise<Program>((resolve, reject) => {
+
+      if (program.id === undefined) {
+        ProgramDAO.create(program).then((biResponse) => {
+          const result: any = biResponse.result;
+          const newProgram: Program = new Program(result.id, result.name);
+          resolve(newProgram);
+
+        }).catch((error) => reject(error));
+      }
+      else {
+        reject();
+      }
+
+    });
   }
 
   static update(program: Program): Promise<Program> {
     //TODO: Check everything is good
-    if (program.id != undefined){
-      return ProgramDAO.update(program.id, program);
-    }
-    else {
-      alert('throwing a fit');
-      return new Promise<any>(() => {});
-    }
+    return new Promise<Program>((resolve, reject) => {
+
+      if (program.id) {
+        ProgramDAO.update(program.id, program).then((biResponse) => {
+          const result: any = biResponse.result;
+          const newProgram: Program = new Program(result.id, result.name, result.species.id);
+          resolve(newProgram);
+
+        }).catch((error) => reject(error));
+      }
+      else {
+        reject();
+      }
+
+    });
   }
 
   static archive(id: string): Promise<Program> {
     //TODO: Check everything is good
-    return ProgramDAO.archive(id);
+    return new Promise<any>(((resolve, reject) => {
+
+      if (id){
+        return ProgramDAO.archive(id)
+          .then(() => resolve())
+          .catch((error) => reject(error));
+      } else {
+        reject();
+      }
+
+    }));
   }
 
   static getAll(): Promise<Program[]> {
-    return ProgramDAO.getAll();
+    return new Promise<Program[]>(((resolve, reject) => {
+
+      ProgramDAO.getAll().then((biResponse) => {
+
+        let programs: Program[] = [];
+    
+        // TODO: workaround for no programs for now
+        if (biResponse.result.data) {
+          // Parse our programs into the vue programs param
+          programs = biResponse.result.data.map((program: any) => {
+            return new Program(program.id, program.name, program.species.id);
+          });
+        }
+    
+        resolve(programs);
+    
+      }).catch((error) => reject(error));
+    
+    }));
   }
 
 }
+
