472683f7ddc8f4c9733f0908dcb5aa5b39e257f7
Nick Palladino <np398@cornell.edu>
2020-05-15 14:25:02 -0400

    [INF-99] Refactored program locations table

--- src/breeding-insight/model/ProgramLocation.ts
+++ src/breeding-insight/model/ProgramLocation.ts
@@ -2,10 +2,12 @@ export class ProgramLocation {
   id?: string;
   programId?: string;
   name?: string;
+  numExperiments?: string;
 
-  constructor(id?: string, programId?: string, name?: string) {
+  constructor(id?: string, programId?: string, name?: string, numExperiments?: string) {
     this.id = id;
     this.programId = programId;
     this.name = name;
+    this.numExperiments = numExperiments;
   }
 }
