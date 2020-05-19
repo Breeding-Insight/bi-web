b39cba0e86a7d8872d237e3f5e3706675ca1e891
Chris T <ct447@cornell.edu>
2020-05-19 09:43:27 -0400

    [INF-158] login failed page changes

--- src/views/LoginFailed.vue
+++ src/views/LoginFailed.vue
@@ -3,20 +3,12 @@
     <div class="card">
       <div class="card-content">
         <h1 class="title has-text-danger">Unable to log you in</h1>
-        <p>We were not able to log you into our system.</p>
-        <p>You could be experiencing this issue for the following reasons:</p>
-        <ul class="is-left" style="list-style-position: inside">
-          <li>The head of your program has not added you to their program</li>
-          <li>Your account has been made inactive in the system.</li>
-          <li>You did not link your Breeding Insight account to your OrcID account
-            <ul>
-              <li>
-                If you did not receive an email for this action, contact the head of your program to
-                receive another invite.
-              </li>
-            </ul>
-          </li>
+        <p>You could be experiencing this issue for one of the following reasons:</p>
+        <ul>
+          <li>You have not been added to this program.</li>
+          <li>Your Breeding Insight account has been deactivated.</li>
         </ul>
+        <p>To resolve this issue, contact the program to send a new invite or reactivate your account.</p>
         <router-link v-bind:to="{name:'home'}">
           Return to Home Page
         </router-link>
