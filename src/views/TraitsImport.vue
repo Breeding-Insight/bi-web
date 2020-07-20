aa22e0265539e53ee465f22ea5f003aea38f5c70
Nick Palladino <np398@cornell.edu>
2020-07-20 11:20:09 -0400

    [ONT-21] Broke out message box components and added xstate

--- src/views/TraitsImport.vue
+++ src/views/TraitsImport.vue
@@ -17,7 +17,7 @@
 
 <template>
   <div class="traits-import">
-    <WarningModal
+    <warning-modal
       v-bind:active.sync="showAbortModal"
       v-bind:msg-title="'Abort This Import'"
       v-on:deactivate="showAbortModal = false"
@@ -43,98 +43,30 @@
           </button>
         </div>
       </div>              
-    </WarningModal>
-    <template v-if="state === State.CHOOSE_FILE || state === State.FILE_CHOSEN">
+    </warning-modal>
+
+    <section v-if="state === ImportState.CHOOSE_FILE || state === ImportState.FILE_CHOSEN">
       <h1 class="title">Import Traits</h1>
-      <article class="message is-info">
-        <div class="message-body">
-          <nav class="level">
-            <div class="level-left">
-              <div class="level-item">
-                <div class="has-text-dark">
-                  <strong>Before You Import...</strong>
-                  <br/>Prepare trait information for import using the provided template.
-                </div>
-              </div>
-            </div>
-            <div class="level-right">
-              <div class="level-item">
-                <div class="has-text-dark has-text-centered is-size-7">
-                  <!-- temporary link until the backend card is done, need github access to our private repo -->
-                  <a href="https://github.com/Breeding-Insight/requirements/raw/master/file-formats/bi_traits_import_template_v01.xls" 
-                    class="button is-outlined is-primary">Download the Trait Import Template</a>
-                  <br/>Template version placeholder
-                </div>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </article>
-      <div class="box">
-        <article>
-          <nav class="level">
-            <div class="level-left">
-              <div v-if="file" class="level-item">
-                <div class="has-text-dark">
-                  {{file.name}}                  
-                </div>
-              </div>
-              <div class="level-item">
-                <div class="has-text-dark">
-                  <file-selector v-model="file"
-                                  v-bind:fileTypes="'.csv, .xls, .xlsx'">
-                  </file-selector>
-                </div>
-              </div>
-            </div>
-            <div class="level-right">
-              <div class="level-item">
-                <div>
-                  <a v-if="state === State.FILE_CHOSEN" class="button is-primary has-text-weight-bold" v-on:click="upload">Import</a>
-                </div>
-              </div>
-            </div>
-          </nav>
-        </article>
-      </div>
+      <trait-import-template-message-box/>
+      
+      <file-select-message-box v-model="file" 
+                               v-bind:fileTypes="'.csv, .xls, .xlsx'"
+                               v-on:import="importService.send(ImportEvent.IMPORT_STARTED)"/>
+    </section>
+    
+    <template v-if="state === ImportState.IMPORTING || state === ImportState.LOADING">
+      <h1 class="title">Importing...</h1>
+      <importing-message-box v-bind:file="file" v-on:abort="importService.send(ImportEvent.ABORT_IMPORT)"/>
     </template>
-
-    <template v-if="state === State.IMPORTING || state === State.LOADING">
-      <importing-message-box v-bind:file="file" v-on:abort="abort"/>
-    </template>
-
-    <template v-if="state === State.LOADING || state === State.CURATE">
+    
+    <template v-if="state === ImportState.LOADING || state === ImportState.CURATE">
       <template v-if="tableLoaded">
         <h1 class="title">Curate and Confirm New Traits</h1>
-        <article class="message is-success">
-          <div class="message-body">
-            <nav class="level">
-              <div class="level-left">
-                <div class="level-item">
-                  <div class="has-text-dark">
-                    <strong>{{numTraits}} new traits and duplicates not checked yet</strong>
-                    <br/>Duplicate traits, highlighted in yellow and a X icon, will not be imported.
-                    <br/>Traits in this list can be directly edited using the "Show details" link.
-                  </div>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="level-item">
-                  <div>
-                    <button class="button is-success has-text-weight-bold" v-on:click="confirm">Confirm</button>
-                  </div>
-                </div>
-                <div class="level-item">
-                  <div>
-                    <button class="button is-outlined" v-on:click="showAbortModal = true">Abort</button>
-                  </div>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </article>
+        <confirm-import-message-box v-bind:num-traits="numTraits" 
+                                    v-on:abort="showAbortModal = true" 
+                                    v-on:confirm="importService.send(ImportEvent.CONFIRMED)"/>
       </template>
-      <traits-import-table v-on:loaded="tableLoadedComplete"/>
+      <traits-import-table v-on:loaded="importService.send(ImportEvent.TABLE_LOADED)"/>
     </template>
 
   </div>
@@ -145,37 +77,54 @@
   import { mapGetters } from 'vuex'
 
   import ProgramsBase from "@/components/program/ProgramsBase.vue"
-  import FileSelector from '@/components/forms/FileSelector.vue'
   import TraitsImportTable from "@/components/trait/TraitsImportTable.vue";
-  import ImportingMessageBox from "@/components/trait/ImportingMessageBox.vue";
+  import ImportingMessageBox from "@/components/file-import/ImportingMessageBox.vue";
+  import ConfirmImportMessageBox from "@/components/trait/ConfirmImportMessageBox.vue";
+  import TraitImportTemplateMessageBox from "@/components/trait/TraitImportTemplateMessageBox.vue";
+  import FileSelectMessageBox from "@/components/file-import/FileSelectMessageBox.vue"
   import WarningModal from '@/components/modals/WarningModal.vue'
 
   import {ProgramUpload} from '@/breeding-insight/model/ProgramUpload'
   import {Program} from '@/breeding-insight/model/Program'
   import {TraitUploadService} from "@/breeding-insight/service/TraitUploadService";
-  
-  enum State {
-    CHOOSE_FILE,
-    FILE_CHOSEN,
-    IMPORTING,
-    LOADING,
-    CURATE
+
+  import { createMachine, interpret } from '@xstate/fsm';
+
+  enum ImportState {
+    CHOOSE_FILE = "CHOOSE_FILE",
+    FILE_CHOSEN = "FILE_CHOSEN",
+    IMPORTING = "IMPORTING",
+    LOADING = "LOADING",
+    CURATE = "CURATE"
   }
 
-  enum Event {
-    FILE_SELECTED,
-    IMPORT_STARTED,
-    ABORT_IMPORT,
-    IMPORT_SUCCESS,
-    IMPORT_ERROR,
-    TABLE_LOADED
+  enum ImportEvent {
+    FILE_SELECTED = "FILE_SELECTED",
+    IMPORT_STARTED = "IMPORT_STARTED",
+    ABORT_IMPORT = "ABORT_IMPORT",
+    IMPORT_SUCCESS = "IMPORT_SUCCESS",
+    CONFIRMED = "CONFIRMED",
+    IMPORT_ERROR = "IMPORT_ERROR",
+    TABLE_LOADED = "TABLE_LOADED"
+  }
+
+  enum ImportAction {
+    RESET = "RESET",
+    START = "START",
+    LOADED = "LOADED",
+    CONFIRM = "CONFIRM",
+    ABORT = "ABORT",
+    STOP_LOADING = "STOP_LOADING",
+    DELETE = "DELETE"
   }
 
   @Component({
     components: {
-      FileSelector,
       TraitsImportTable,
       ImportingMessageBox,
+      ConfirmImportMessageBox,
+      FileSelectMessageBox,
+      TraitImportTemplateMessageBox,
       WarningModal
     },
     computed: {
@@ -186,111 +135,154 @@
   })
   export default class TraitsImport extends ProgramsBase {
 
-    private State = State;
-    private Event = Event;
-    private state: State = State.CHOOSE_FILE;
     private file : File | null = null;
     private activeProgram?: Program;
     private tableLoaded = false;
     private numTraits = 0;
     private showAbortModal = false;
+    
+    private ImportState = ImportState;
+    private ImportEvent = ImportEvent;
+    private ImportAction = ImportAction;
+
+    private state = ImportState.CHOOSE_FILE;
+    private importStateMachine = createMachine({
+      id: 'import',
+      initial: ImportState.CHOOSE_FILE,
+      states: {
+        [ImportState.CHOOSE_FILE]: {
+          entry: ImportAction.RESET,
+          on: { 
+            [ImportEvent.FILE_SELECTED]: ImportState.FILE_CHOSEN 
+          } 
+        },
+        [ImportState.FILE_CHOSEN]: { 
+          on: { 
+            [ImportEvent.IMPORT_STARTED]: ImportState.IMPORTING 
+          }
+        },
+        [ImportState.IMPORTING]: {
+          entry: ImportAction.START,
+          on: { 
+            [ImportEvent.ABORT_IMPORT]: {
+              target: ImportState.CHOOSE_FILE,
+              actions: ImportAction.ABORT
+            },
+            [ImportEvent.IMPORT_SUCCESS]: ImportState.LOADING,
+            [ImportEvent.IMPORT_ERROR]: ImportState.CHOOSE_FILE,
+          }
+        },
+        [ImportState.LOADING]: {
+          on: {
+            [ImportEvent.ABORT_IMPORT]: {
+              target: ImportState.CHOOSE_FILE,
+              actions: ImportAction.STOP_LOADING
+            },
+            [ImportEvent.TABLE_LOADED]: {
+              target: ImportState.CURATE,
+              actions: ImportAction.LOADED
+            },
+          }
+        },
+        [ImportState.CURATE]: {
+          on: {
+            [ImportEvent.ABORT_IMPORT]: {
+              target: ImportState.CHOOSE_FILE,
+              actions: ImportAction.DELETE
+            },
+          }
+        }
+      }
+    },
+    {
+      actions: {
+        [ImportAction.RESET]: (context, event) => {
+          this.reset();
+        },
+        [ImportAction.START]: (context, event) => {
+          this.upload();
+        },
+        [ImportAction.LOADED]: (context, event) => {
+          this.loaded();
+        },
+        [ImportAction.ABORT]: (context, event) => {
+          this.abort();
+        },
+        [ImportAction.STOP_LOADING]: (context, event) => {
+          this.stopLoading();
+        },
+        [ImportAction.DELETE]: (context, event) => {
+          this.delete();
+        }
+      }
+
+    });
+
+    private initialState = this.importStateMachine;
+    private importService = interpret(this.importStateMachine);
+
+    created() {
+      this.importService.subscribe(state => {
+        console.log("new state: " + state.value);
+        this.state = ImportState[state.value as keyof typeof ImportState];
+      });
+      this.importService.start();
+    }
 
     @Watch('file')
     onFileChanged(value: string, oldValue: string) {
       if (oldValue === null && value !== null) {
-        this.updateState(Event.FILE_SELECTED);
+        this.importService.send(ImportEvent.FILE_SELECTED);
       }
     }
 
     upload() {
-      this.updateState(Event.IMPORT_STARTED);
       TraitUploadService.uploadFile(this.activeProgram!.id!, this.file!).then((response) => {
         this.numTraits = response.data!.length;
-        this.updateState(Event.IMPORT_SUCCESS);
+        this.importService.send(ImportEvent.IMPORT_SUCCESS);
       }).catch((error) => {
         // proper error handling is not part of ONT-21
         this.$emit('show-error-notification', error.response.statusText);
-        this.updateState(Event.IMPORT_ERROR);
+        this.importService.send(ImportEvent.IMPORT_ERROR);
       });
     }
 
     abort() {
       // TODO: actually cancel request
+      this.showCancelledNotification();
+    }
+
+    stopLoading() {
+      // upload will still be there but didn't want to wait for it to load
+      this.showCancelledNotification();
+    }
+
+    delete() {
+      // TODO: call DELETE /trait-upload
+      this.showCancelledNotification();
+    }
+
+    showCancelledNotification() {
       this.$emit('show-info-notification', 'Import cancelled');
-      this.updateState(Event.ABORT_IMPORT);
     }
 
     handleAbortModal() {
       this.showAbortModal = false;
-      // TODO: call DELETE /trait-upload
-      this.abort();
+      this.importService.send(ImportEvent.ABORT_IMPORT);
     }
 
-    tableLoadedComplete() {
+    loaded() {
       this.tableLoaded = true;
-      this.updateState(Event.TABLE_LOADED);
     }
 
     confirm() {
       //TODO POST /traits
     }
 
-    updateState(event: Event) {
-      console.log(event);
-      switch(this.state) {
-        case State.CHOOSE_FILE:
-          if (event === Event.FILE_SELECTED) {
-            this.state = State.FILE_CHOSEN;
-          }
-          break;
-        case State.FILE_CHOSEN:
-          if (event === Event.IMPORT_STARTED) {
-            this.state = State.IMPORTING;
-          }
-          break;
-        case State.IMPORTING:
-          if (event === Event.ABORT_IMPORT) {
-            this.state = State.CHOOSE_FILE;
-          }
-          else if (event === Event.IMPORT_SUCCESS) {
-            this.state = State.LOADING;
-          }
-          else if (event === Event.IMPORT_ERROR) {
-            // TODO: transition to error page view
-            this.state = State.CHOOSE_FILE;
-          }
-          break;
-        case State.LOADING:
-          if (event === Event.ABORT_IMPORT) {
-            this.state = State.CHOOSE_FILE;
-          }
-          if (event === Event.TABLE_LOADED) {
-            this.state = State.CURATE;
-          }
-          break;
-        case State.CURATE:
-          if (event === Event.ABORT_IMPORT) {
-            this.state = State.CHOOSE_FILE;
-          }
-          break;
-      }
-      this.onEnterActions();
-    }
-
-    onEnterActions() {
-      switch(this.state) {
-        case State.CHOOSE_FILE:
-          this.onEnterChooseFile();
-          break;
-      } 
-    }
-
-    onEnterChooseFile() {
-      console.log('onEnterChooseFile');
+    reset() {
       this.file = null;
       this.tableLoaded = false;
     }
-
 
   }
 </script>
