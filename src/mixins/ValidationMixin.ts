c9ba31161e6c9db1b7c6d5819e43b0e9db4c0214
Nick Palladino <np398@cornell.edu>
2020-08-12 15:43:49 -0400

    [ONT-37] Work on validations but not done

--- src/mixins/ValidationMixin.ts
+++ src/mixins/ValidationMixin.ts
@@ -15,57 +15,62 @@
  * limitations under the License.
  */
 
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 import {Validations} from "vuelidate-property-decorators"
 import {TableRow} from "@/breeding-insight/model/view_models/TableRow"
 
 @Component
 class ValidationMixin extends Vue {
 
-  private currentValidationRow!: TableRow<any>;
+  @Prop()
+  private rowValidations!: Object;
 
-    // if this row based works then make mixin so can reuse and use Vue class functions
+  private currentValidationRow: TableRow<any> | undefined = undefined;
 
-    @Validations()
-    validations() {
-      if (this.rowValidations && this.currentValidationRow ) {
-        return {
-          currentValidationRow: {
-            editData: {
-              ...this.rowValidations
-            }
-          }
+  // if this row based works then make mixin so can reuse and use Vue class functions
+
+  @Validations()
+  validations() {
+    if (this.rowValidations) {
+      return {
+        currentValidationRow: {
+          editData: {
+            ...this.rowValidations
+          } 
         }
       }
-
-      return {}
-    }
-  
-    getValidations(record: TableRow<any>, index: number) {
-      this.currentValidationRow = record;
-      return this.$v.currentValidationRow;
     }
 
-    validateAndSubmit(record: TableRow<any>, rowIndex: number) {
+    return {}
+  }
 
-      this.$v.currentValidationRow.editData.$touch();
-      if (this.$v.currentValidationRow.editData.$anyError){
-        this.$emit('show-error-notification', 'Fix Invalid Fields');
-        return;
-      }
-      else {
-        // Check all of our fields to see if they were required
-        this.$v.currentValidationRow.editData.$reset();
-        const editedRecord = record.editData;
-        this.$emit('submit', editedRecord);
-      }
+  getValidations(record: TableRow<any>) {
+    this.currentValidationRow = record;
+    return this.$v.currentValidationRow.editData;
+  }
+
+  validateAndSubmit(record: TableRow<any>) {
+    this.currentValidationRow = record;
+    this.$v.currentValidationRow.editData.$touch();
+
+    if (this.$v.currentValidationRow.editData.$anyError){
+      this.$emit('show-error-notification', 'Fix Invalid Fields');
+      return;
     }
-
-    cancelEdit(record: TableRow<any>, rowIndex: number) {
-      record.toggleEdit();
-      record.revertChanges();
-      // clear form
+    else {
+      // Check all of our fields to see if they were required
+      
+      const editedRecord = record.editData;
+      this.$emit('submit', editedRecord);
       this.$v.currentValidationRow.editData.$reset();
     }
+  }
+
+  cancelEdit(record: TableRow<any>) {
+    record.toggleEdit();
+    record.revertChanges();
+    // clear form
+    this.$v.currentValidationRow.editData.$reset();
+  }
 }
 export default ValidationMixin
