7ffbb4ddd3b17f61926992e8355c4adf80970420
Chris T <ct447@cornell.edu>
2020-08-19 14:33:51 -0400

    [INF-199] fix/remove existing tests. Configure vue to use jest. Pruned unneeded dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,10 +39,11 @@ module.exports = {
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)'
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
-        mocha: true
+        jest: true
       }
     }
   ]
