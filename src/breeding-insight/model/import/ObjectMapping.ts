9bffb3dd7d420f83f75e726e03ca382ab6953c0a
Chris T <ct447@cornell.edu>
2021-02-24 13:12:44 -0500

    [importer] code cleanup up and bug fixes to importer

--- src/breeding-insight/model/import/ObjectMapping.ts
+++ src/breeding-insight/model/import/ObjectMapping.ts
@@ -13,9 +13,7 @@
 */
 
 import {Mapping} from "@/breeding-insight/model/import/Mapping";
-import { v4 as uuidv4 } from 'uuid';
-import {ImportGroup} from "@/breeding-insight/model/import/ImportGroup";
-import {ImportRelationType} from "@/breeding-insight/model/import/ImportRelation";
+import { v4 as uuidv4 } from "uuid";
 
 export class ObjectMapping {
   id?: string;
@@ -27,7 +25,7 @@ export class ObjectMapping {
     this.id = objectMapping.id ? objectMapping.id : uuidv4();
     if (objectMapping.fields) {
       this.fields = {}
-      Object.keys(objectMapping.fields).forEach(key => this.fields![key] = new Mapping(this.fields![key]));
+      Object.keys(objectMapping.fields).forEach(key => this.fields![key] = new Mapping(objectMapping.fields![key]));
     } else {
       this.fields = {};
     }
@@ -55,39 +53,4 @@ export class ObjectMapping {
   replaceMapping(field: string, newMapping: Mapping) {
     this.fields![field] = newMapping;
   }
-
-  // Set field mapping
-  setFieldMapping(importField: string, fileFieldName: string) {
-    this.fields![importField] = new Mapping({fileFieldName});
-  }
-
-  // Set manual mapping
-  setManualMapping(importField: string, constantValue: string) {
-    this.fields![importField] = new Mapping({constantValue});
-  }
-
-  setRelationType(importField: string, relationValue: ImportRelationType) {
-    this.fields![importField] = new Mapping({relationValue});
-  }
-
-  // Add object to list field
-  addObjectToListField(targetField: string, importGroup: ImportGroup) {
-    if (!this.fields![targetField]){
-      this.fields![targetField] = new Mapping({});
-    }
-    // Create new object
-    const newObject = new ObjectMapping({object_id: importGroup.id} as ObjectMapping);
-    this.fields![targetField].objects!.push(newObject);
-    return { config: importGroup, object: newObject}
-  }
-
-  getObjectsFromListField(field: string): ObjectMapping[] {
-    if (this.fields![field]) {
-      return this.fields![field].objects!;
-    } else {
-      return [];
-    }
-  }
-
-
 }
