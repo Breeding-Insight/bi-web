f22487c544a199e8ac2d310d364044227cfb3349
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2021-06-02 23:21:30 -0400

    [BI-750] add study dao

---
+++ src/breeding-insight/dao/StudyDAO.ts
@@ -0,0 +1,48 @@
+/*
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Study} from "@/breeding-insight/model/Study";
+import {BiResponse, Response} from "@/breeding-insight/model/BiResponse";
+import * as api from "@/util/api";
+import {PaginationQuery} from "@/breeding-insight/model/PaginationQuery";
+
+export class StudyDAO {
+
+  static async getAll(programId: string, trialId: string, paginationQuery: PaginationQuery, full : boolean): Promise<BiResponse> {
+    try {
+      const query = {
+        page: 0,
+        pageSize: 50,
+        trialDbIds: [
+          trialId
+        ]
+      };
+      
+      const { data } = await api.call({
+        url: `${process.env.VUE_APP_BI_API_V1_PATH}/programs/${programId}/brapi/v2/search/studies`,
+        method: 'post',
+        data: query,
+        params: { full }
+      });
+        
+      return new BiResponse(data);
+        
+    } catch (err) {
+      throw err;
+    }  
+  }    
+}
