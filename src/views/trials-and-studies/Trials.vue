0fb0915740c0bd2c90c4b59a0508abc6086b717b
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2021-06-17 13:03:31 -0400

    [BI-750] add studies list table

---
+++ src/views/trials-and-studies/Trials.vue
@@ -0,0 +1,42 @@
+<!--
+  - See the NOTICE file distributed with this work for additional information
+  - regarding copyright ownership.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+
+<template>
+  <div id="trials">
+    <h1 class="title">All Trials</h1>
+    <TrialsTable
+      v-on:show-success-notification="$emit('show-success-notification', $event)"
+      v-on:show-error-notification="$emit('show-error-notification', $event)"
+    >
+    </TrialsTable>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator'
+  import TrialsTable from "@/components/trials/TrialsTable.vue"
+  import TrialsAndExperimentsBase from "@/components/trials/TrialsAndExperimentsBase.vue";
+
+  @Component({
+    components: {
+      TrialsTable
+    }
+  })
+  export default class Trials extends TrialsAndExperimentsBase {
+
+  }
+</script>
