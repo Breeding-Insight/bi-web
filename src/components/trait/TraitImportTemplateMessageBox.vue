1c601eea6081fbfc7ff5a230c9b5191f9028ad3e
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2021-08-12 10:18:42 -0400

    [BI-982] update trait import template link to point to version 10

--- src/components/trait/TraitImportTemplateMessageBox.vue
+++ src/components/trait/TraitImportTemplateMessageBox.vue
@@ -32,7 +32,7 @@
             <div class="level-item">
               <div class="has-text-dark has-text-centered is-size-7">
                 <!-- temporary link until the backend card is done -->
-                <a href="https://cornell.box.com/shared/static/8sp0qvccpjotosiv8576tczeg09nnvao.xls"
+                <a href="https://cornell.box.com/shared/static/u9vuob3vvu2cgwyoe26b2722u41b8f6o.xls"
                   class="button is-outlined is-primary" :id="downloadTraitTemplateId">Download the Trait Import Template</a>
                 <br/>Template version placeholder
               </div>
