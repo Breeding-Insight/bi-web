262ca89d0f482ba338190b2bc4f74b9202a093e3
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2021-09-24 14:34:52 -0400

    [BI-1031] make model for trait search filters

---
+++ src/breeding-insight/model/TraitSelector.ts
@@ -0,0 +1,41 @@
+enum TraitField {
+  NAME = 'name',
+  ABBREVIATIONS = 'abbreviations',
+  MAIN_ABBREVIATION = 'mainAbbreviation',
+  SYNONYMS = 'synonyms',
+  LEVEL = 'level',
+  STATUS = 'status',
+  METHOD_DESCRIPTION = 'methodDescription',
+  METHOD_CLASS = 'methodClass',
+  METHOD_FORMULA = 'methodFormula',
+  SCALE_NAME = 'scaleName',
+  SCALE_CLASS = 'scaleClass',
+  SCALE_DECIMAL_PLACES = 'scaleDecimalPlaces',
+  SCALE_LOWER_LIMIT = 'scaleLowerLimit',
+  SCALE_UPPER_LIMT = 'scaleUpperLimit',
+  SCALE_CATEGORIES = 'scaleCategories',
+  CREATED_AT = 'createdAt',
+  UPDATED_AT = 'updatedAt',
+  CREATED_BY_USER_ID = 'createdByUserId',
+  CREATED_BY_USER_NAME = 'createdByUserName',
+  UPDATED_BY_USER_ID = 'updatedByUserId',
+  UPDATED_BY_USER_NAME = 'updatedByUserName'
+}
+
+export class Filter {
+  field?: TraitField;
+  value?: string | number;
+}
+
+export class TraitSelector {
+  filters: Filter[]
+
+  constructor(filters: Filter[] = []) {
+    this.filters = filters;
+  }
+
+  addFilter(filter: Filter) {
+    this.filters.push(filter);
+  }
+
+}
