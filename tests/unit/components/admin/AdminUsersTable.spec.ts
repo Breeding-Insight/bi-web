8eada0b0c6e401f7513af8a5e88afcffeac351f9
David Randolph Phillips <drp227@cornell.edu>
2021-10-11 15:17:12 -0400

    [BI-760] recommented out 1-lines of test

--- tests/unit/components/admin/AdminUsersTable.spec.ts
+++ tests/unit/components/admin/AdminUsersTable.spec.ts
@@ -76,7 +76,7 @@ describe('new data form works properly', () => {
       expect(saveBtn.exists()).toBeTruthy();
       await saveBtn.trigger('click');
       //
-  //    await Utils.pause(500);
+      await Utils.pause(500);
       await wrapper.vm.$nextTick();
       // Wait another DOM update. A little hacky, probably should find better way to do this in the future.
       await wrapper.vm.$nextTick();
