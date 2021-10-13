29fc5de8f421b4ea4a8371211fa77b4a309f15b9
David Randolph Phillips <drp227@cornell.edu>
2021-10-13 11:17:17 -0400

    [BI-780] shortened pause from 500 to 50

--- tests/unit/components/admin/AdminUsersTable.spec.ts
+++ tests/unit/components/admin/AdminUsersTable.spec.ts
@@ -69,7 +69,7 @@ describe('new data form works properly', () => {
     expect(saveBtn.exists()).toBeTruthy();
     await saveBtn.trigger('click');
     console.log("---before pause---");
-    await Utils.pause(500);
+    await Utils.pause(50);
     console.log("---after pause---");
     await wrapper.vm.$nextTick();
     // Wait another DOM update. A little hacky, probably should find better way to do this in the future.
