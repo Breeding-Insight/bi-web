765c1142193b31bfc6b829b9aa0e423f4b565153
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2021-10-20 15:28:25 -0400

    [BI-1031] fix typo

--- src/components/tables/expandableTable/ExpandableTable.vue
+++ src/components/tables/expandableTable/ExpandableTable.vue
@@ -181,7 +181,7 @@ export default class ExpandableTable extends Mixins(ValidationMixin) {
       }
       rowArray.push(newTableRow);
     }
-    this. tableRows = rowArray;
+    this.tableRows = rowArray;
   }
 
   cancelEditClicked(row:any) {
