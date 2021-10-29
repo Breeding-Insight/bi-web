02c97c5ed3c65e9b4fe35c2a1e383653e579b6d5
Nick Palladino <np398@cornell.edu>
2021-10-29 11:33:55 -0400

    [BI-1175] Start reusable import template and use for germplasm

--- src/views/import/ImportGermplasm.vue
+++ src/views/import/ImportGermplasm.vue
@@ -17,22 +17,23 @@
 
 <template>
   <div id="import-germplasm">
-    <ImportInfoTemplateMessageBox v-bind:import-type-name="'Germplasm'"
-                                  v-bind:template-url="'https://cornell.box.com/shared/static/kv3l900otsea9zm3iowp7e9umpcjvsck.xls'"
-                                  class="mb-5">
-      <strong>Before You Import...</strong>
-      <br/>
-      Any file containing germplasm names, breeding methods, and sources can be imported regardless of header format.
-      The system provides guidance for matching file column headers to their appropriate database location.
-      If you use the headers specified in the import template, matching will be automatic.
-      Any germplasm detail (attribute or passport information) not specified in the template can be added to the
-      database via customized matching in the upcoming step.
-    </ImportInfoTemplateMessageBox>
-    <div class="box">
-      <FileSelectMessageBox v-model="file"
-                            v-bind:fileTypes="'.csv, .xls, .xlsx'"
-      />
-    </div>
+    <ImportTemplate v-bind:abort-msg="'No germplasm records will be added, and the import in progress will be completely removed.'"
+                    v-on:upload-file="upload($event)"
+                    v-on="$listeners">
+      <template v-slot:importInfoTemplateMessageBox>
+        <ImportInfoTemplateMessageBox v-bind:import-type-name="'Germplasm'"
+                                      v-bind:template-url="'https://cornell.box.com/shared/static/kv3l900otsea9zm3iowp7e9umpcjvsck.xls'"
+                                      class="mb-5">
+          <strong>Before You Import...</strong>
+          <br/>
+          Any file containing germplasm names, breeding methods, and sources can be imported regardless of header format.
+          The system provides guidance for matching file column headers to their appropriate database location.
+          If you use the headers specified in the import template, matching will be automatic.
+          Any germplasm detail (attribute or passport information) not specified in the template can be added to the
+          database via customized matching in the upcoming step.
+        </ImportInfoTemplateMessageBox>
+      </template>
+    </ImportTemplate>
   </div>
 </template>
 
@@ -40,15 +41,17 @@
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import ProgramsBase from "@/components/program/ProgramsBase.vue";
 import ImportInfoTemplateMessageBox from "@/components/file-import/ImportInfoTemplateMessageBox.vue";
-import FileSelectMessageBox from "@/components/file-import/FileSelectMessageBox.vue";
+import ImportTemplate from "@/views/import/ImportTemplate.vue";
 
 @Component({
-  components: {ImportInfoTemplateMessageBox, FileSelectMessageBox
+  components: {ImportInfoTemplateMessageBox, ImportTemplate
   }
 })
 export default class ImportGermplasm extends ProgramsBase {
 
-  private file : File | null = null;
+  upload() {
+    console.log('upload');
+  }
 
 }
 </script>
