ec06d236aecd39795055cc91d3fbd6a51dba4a7f
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2021-11-11 10:30:06 -0500

    [BI-623] manage sort order state globally

---
+++ src/store/sorting/mutation-types.ts
@@ -0,0 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const NEW_SORT_COLUMN = 'newSortColumn';
+export const TOGGLE_NAME_SORT_ORDER = 'toggleNameSortOrder';
+export const TOGGLE_METHOD_SORT_ORDER = 'toggleMethodSortOrder';
+export const TOGGLE_SCALE_CLASS_SORT_ORDER = 'toggleScaleClassSortOrder';
+export const TOGGLE_UNIT_SORT_ORDER = 'toggleUnitSortOrder';
