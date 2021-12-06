4eafdc9010da3581919696bd030f5f5826ccb9a3
Tim Parsons <tep46@cornell.edu>
2021-12-06 09:25:39 -0500

    [merge-0.4] Removing duplicate property declarations

--- src/components/admin/AdminProgramsTable.vue
+++ src/components/admin/AdminProgramsTable.vue
@@ -295,11 +295,6 @@ export default class AdminProgramsTable extends Vue {
     this.$store.commit(DEACTIVATE_ALL_NOTIFICATIONS);
   }
 
-  programEditValidations = {
-    name: {required},
-    speciesId: {required}
-  }
-
   mounted() {
     this.getPrograms();
     this.getSpecies();
