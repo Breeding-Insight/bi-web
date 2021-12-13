b7f189948ed36f322c5670461b6bd841b6bb591e
HMS17 <84345306+HMS17@users.noreply.github.com>
2021-12-13 17:28:11 -0500

    [BI-1086] Germplasm lists table 2

--- src/components/germplasm/GermplasmListsTable.vue
+++ src/components/germplasm/GermplasmListsTable.vue
@@ -19,7 +19,7 @@
   <section id="traitTableLabel">
     <ExpandableTable
         v-bind:records="germplasmLists"
-        v-bind:pagination="germplasmListsPagination"
+        v-bind:pagination="GermplasmListsPagination"
         v-bind:auto-handle-close-panel-event="false"
         v-on:paginate="paginationController.updatePage($event)"
         v-on:paginate-toggle-all="paginationController.toggleShowAll()"
