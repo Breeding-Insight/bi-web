a0564f8fe9ac7292ccffdf172c9b97dc38b9ec0a
Nick Palladino <np398@cornell.edu>
2022-04-28 10:15:44 -0400

    [hackathon-data-analysis] Added select fields to layout

--- src/components/observations/ObservationsPlot.vue
+++ src/components/observations/ObservationsPlot.vue
@@ -16,15 +16,34 @@
   -->
 
 <template>
-  <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+  <div>
+    <div class="columns">
+      <div class="column is-one-half">
+        <BasicSelectField
+            v-model="selectedVariable"
+            v-bind:options="variables"
+            v-bind:field-name="'Variable'"
+        />
+      </div>
+      <div class="column is-one-half">
+        <BasicSelectField
+            v-model="selectedPlotType"
+            v-bind:options="plotTypes"
+            v-bind:field-name="'Plot Type'"
+        />
+      </div>
+    </div>
+    <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+  </div>
 </template>
 
 <script lang="ts">
 import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
 import { Plotly } from 'vue-plotly'
+import BasicSelectField from "@/components/forms/BasicSelectField.vue";
 
 @Component({
-      components: {Plotly}
+      components: {Plotly, BasicSelectField}
     }
 )
 
@@ -39,6 +58,11 @@ export default class ObservationsPlot extends Vue {
   private layout = {
     title: "My graph"
   };
+
+  private variables = ["test1", "test2"];
+  private plotTypes = ["Scatter Plot", "Histogram"];
+  private selectedVariable : string | undefined;
+  private selectedPlotType : string | undefined;
 
 }
 
