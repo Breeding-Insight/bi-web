e321042fbeec9d487bed6eb6f8c04c5818eee16b
David Randolph Phillips <drp227@cornell.edu>
2024-05-20 15:54:23 -0400

    [BI-2105]  made clear that the customized css was for accessibilty

--- src/components/layouts/UserSideBarLayout.vue
+++ src/components/layouts/UserSideBarLayout.vue
@@ -78,7 +78,7 @@
     <template v-slot:menu>
       <nav role="navigation" aria-label="main navigation">
         <template v-if="activeProgram">
-          <p class="menu-label customize">
+          <p class="menu-label customize_for_accessibility">
             {{activeProgram.name}}
           </p>
           <ul class="menu-list">
@@ -165,7 +165,7 @@
           <template v-if="activeProgram">
             <hr style="margin:5px;">
           </template>
-          <p class="menu-label customize">
+          <p class="menu-label customize_for_accessibility">
             System Administration
           </p>
           <ul class="menu-list">
