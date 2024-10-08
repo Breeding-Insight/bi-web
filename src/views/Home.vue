bf7f3f865769fdde7290083efd887714cf38151f
Nick <53413353+nickpalladino@users.noreply.github.com>
2024-10-08 11:35:31 -0400

    Removed user side panel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,28 +19,8 @@
   <div class="userhome">
     <div class="is-divider"></div>
     <div class="columns">
-      <div class="column is-three-fifths">
+      <div class="column is-full">
         <p class="title">Welcome, {{ activeUser.name }}!</p>
-      </div>
-      <div class="column">
-        <div class="card">
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                </figure>
-              </div>
-              <div class="media-content">
-                <p class="title is-4">{{ activeUser.name }}</p>
-                <p class="subtitle is-6">{{ activeUser.orcid }}</p>
-              </div>
-            </div>
-          </div>
-<!--          <footer class="card-footer">-->
-<!--            <a href="#" class="card-footer-item">Edit</a>-->
-<!--          </footer>-->
-        </div>
       </div>
     </div>
   </div>
