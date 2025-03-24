557d6191bf0e0242fec011c0528a1798b8c19ac5
mlm483 <128052931+mlm483@users.noreply.github.com>
2025-03-24 16:56:00 -0400

    [BI-2574] - updated import template links

--- src/views/import/ImportExperiment.vue
+++ src/views/import/ImportExperiment.vue
@@ -33,7 +33,7 @@
 
       <template v-slot:importInfoTemplateMessageBox>
         <ImportInfoTemplateMessageBox v-bind:import-type-name="'Experiments & Observations'"
-                                      v-bind:template-url="'https://cornell.box.com/shared/static/7ziyhhnia5i7bdvekawhb2ldyobrecmw.xls'"
+                                      v-bind:template-url="'https://cornell.box.com/shared/static/x53n1x8fhccgij8hroel0zuz3y4l31iu.xls'"
                                       class="mb-5">
           <strong>Before You Import...</strong>
           <br/>
