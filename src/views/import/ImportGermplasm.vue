37bc32bf1a424153034bc6b4ffcb4cc79d9941cd
Keerthi Humsika Kattamudi <humsikak2000@gmail.com>
2026-05-01 18:45:57 -0400

    [BI-2844] BI-2844 - Updated germplasm import template download link to v12 reference.

--- src/views/import/ImportGermplasm.vue
+++ src/views/import/ImportGermplasm.vue
@@ -31,7 +31,7 @@
 
       <template v-slot:importInfoTemplateMessageBox>
         <ImportInfoTemplateMessageBox v-bind:import-type-name="'Germplasm'"
-                                      v-bind:template-url="'https://www.dropbox.com/scl/fi/sezsc80x7vxaeor7i8vvn/bi_germplasm_import_template_v11.xls?rlkey=91gf971xi293zqn2x7ago1223&st=46nu0kte&dl=1'"
+                                      v-bind:template-url="'https://www.dropbox.com/scl/fi/qavhqq8tzxr058cldqzgw/bi_germplasm_import_template_v12.xls?rlkey=efxt6a0c441xd65x9zvvtko9n&st=5y2692no&dl=1'"
                                       class="mb-5">
           <strong>Before You Import...</strong>
           <br/>
